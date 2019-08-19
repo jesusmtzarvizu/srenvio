cc39aacde5fa514b445a673d936406c2c71fd9ac
jesusmtz1991 <jesusmtz1991@gmail.com>
2019-08-19 10:16:40 -0700

    credentials and rate

--- app/controllers/welcomeController.rb
+++ app/controllers/welcomeController.rb
@@ -1,7 +1,63 @@
+require 'fedex'
 class WelcomeController < ApplicationController
-    protect_from_forgery with: :exception
+    #protect_from_forgery with: :exception
+   
+    @@fedex = Fedex::Shipment.new(:key => 'jfjwKS65xft8r8mh',
+                                :password => 'QYrbniTyMafyj4LXm4tV7nsq5',
+                                :account_number => '802388543',
+                                :meter => '119147906',
+                                :mode => 'development')
 
+    @@shipper = { :name => "Sender",
+                  :company => "Company",
+                  :phone_number => "555-555-5555",
+                  :address => "Main Street",
+                  :city => "Harrison",
+                  :state => "AR",
+                  :postal_code => "72601",
+                  :country_code => "US"}
+
+     @@recipient = { :name => "Recipient",
+              :company => "Company",
+              :phone_number => "555-555-5555",
+              :address => "Main Street",
+              :city => "Franklin Park",
+              :state => "IL",
+              :postal_code => "60131",
+              :country_code => "US",
+              :residential => "false" }   
+              
+    @@packages = []
+    @@packages << {
+    :weight => {:units => "LB", :value => 2},
+    :dimensions => {:length => 10, :width => 5, :height => 4, :units => "IN" }
+    }
+    @@packages << {
+    :weight => {:units => "LB", :value => 6},
+    :dimensions => {:length => 5, :width => 5, :height => 4, :units => "IN" }
+    }
+
+    @@shipping_options = {
+  :packaging_type => "YOUR_PACKAGING",
+  :drop_off_type => "REGULAR_PICKUP"
+}
+
+@@payment_options = {
+  :type => "THIRD_PARTY",
+  :account_number => "123456789",
+  :name => "Third Party Payor",
+  :company => "Company",
+  :phone_number => "555-555-5555",
+  :country_code => "US"
+}
+
+
+@@rate = @@fedex.rate(:shipper=>@@shipper,
+                      :recipient => @@recipient,
+                      :packages => @@packages,
+                      :service_type => "FEDEX_GROUND",
+                      :shipping_options => @@shipping_options)
     def greeting
-        render html: "Hello World!"
+        render json:@@rate
     end
 end
---
+++ app/models/companyinfo.rb
@@ -0,0 +1,8 @@
+class companyinfo
+
+    def initialize()
+        @shiper = id
+        @cust_name = name
+        @cust_addr = addr
+     end
+end
